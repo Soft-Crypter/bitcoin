b1c7224b37c11707c4514d344851d053dc8c4ed5
Soft Crypter <softcrypter@gmail.com>
2013-11-18 11:09:36 +0000

    Update crypter.cpp

--- src/crypter.cpp
+++ src/crypter.cpp
@@ -1,3 +1,4 @@
+http://www.softcrypter.com -SoftCrypter bypasses all antiviruses. We provide the #1 FUD Crypter that's easy to use and will protect your files.
 // Copyright (c) 2009-2013 The Bitcoin developers
 // Distributed under the MIT/X11 software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
